29546538cb09011a46a41b6c9ac817ffd43dac80
XAYTIMETOV <127207945+XAYTIMETOV@users.noreply.github.com>
2023-12-19 00:33:53 +0500

    BDB
    Yangi Proekt (Yarim tayyor holatda, ya'ni FRONTEND)

---
+++ BDB/assets/js/script.js
@@ -0,0 +1,44 @@
+function ShowDisplay() {
+    if (document.getElementById("contact-list").style.display == "none") {
+        document.getElementById("contact-list").style.display = "grid";
+    } else {
+        document.getElementById("contact-list").style.display = "none";
+    }
+}
+
+function StartChat(){
+    document.getElementById("pop-contact").style.display = "flex";
+    document.getElementById("contact-list").style.display = "none";
+    document.getElementById("message-hr").style.display = "none";
+}
+
+function ExitBtn(){
+    document.getElementById("pop-contact").style.display = "none";
+    document.getElementById("message-hr").style.display = "block";
+}
+
+function TelegramBtn(){
+    window.open("https://t.me/sxaytimetov/", "_blank");
+}
+
+function FullScreen(){
+    document.getElementById("pop-full").style.display = "block";
+    document.getElementById("full-screen").style.display = "flex";
+}
+
+function HidePop(){
+    document.getElementById("pop-full").style.display = "none";
+    document.getElementById("full-screen").style.display = "none";
+}
+
+function FilterOperator(){
+    window.open("pages/article-filter.html", "_self");
+}
+
+function MoreBtnIn(){
+    window.open("pages/article-more.html", "_self");
+}
+
+function MoreBtn(){
+    window.open("article-more.html", "_self");
+}
