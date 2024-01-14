436944d030dd7ffa77d367ecc9bf15289b4217c2
Jamshidbek <jamshidbekollanazarov@gmail.com>
2024-01-13 23:15:44 -0800

    comm

--- operators/admin.py
+++ operators/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 from .models import Operator
+from django.utils.html import format_html
 
 
 @admin.register(Operator)
 class OperatorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug', 'photo', 'date')
-    readonly_fields = ['img_preview', ]
+    list_display = ('name', 'slug', 'photo', 'color_column', 'date')
+    readonly_fields = ['img_preview']
